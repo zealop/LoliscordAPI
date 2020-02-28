cb8cc9e6db37bafe068ef12ccaf853a63dee85b8
Phong Trần <phong.tran.maple@gmail.com>
2020-02-28 13:31:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var app = express();
+var PORT = process.env.PORT || 8000
 app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
-app.listen(8000, () => {
- console.log("Server running on port 8000");
+app.listen(PORT, () => {
+  console.log("Server running on port %s", PORT);
 });
