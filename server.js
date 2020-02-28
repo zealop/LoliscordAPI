c9de686518618f3014f22f9bbfb474e254c0a30f
Phong Trần <phong.tran.maple@gmail.com>
2020-02-28 14:07:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8000
-app.get("/", (req, res) => {
+var PORT = process.env.PORT || 8000;
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://loliscord.herokuapp.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get("/", (req, res, next) => {
   res.send("hello world");
 });
 
