99cb6440956c6ea9b11d2f5bff8f065651b37109
Maple <phong.tran.maple@hcmut.edu.vn>
2020-05-07 10:47:35 +0700

    User data update

--- server.ts
+++ server.ts
@@ -1,22 +1,27 @@
-"use strict";
-exports.__esModule = true;
-var PORT = process.env.PORT || 8000;
-var express = require("express");
-var app = express();
-var http = require("http");
-var server = http.createServer(app);
-var cors = require("cors");
+const  PORT = process.env.PORT || 8000;
+
+import express = require('express');
+const app: express.Application = express();
+
+import http = require('http');
+const server: http.Server = http.createServer(app);
+
+import cors = require('cors');
 app.use(cors());
-var io = require('socket.io').listen(server);
+
+const io  = require('socket.io').listen(server);
 io.origins('*:*');
-server.listen(PORT, null, function () {
+
+server.listen(PORT, null, () => {
     console.log("Listening on port " + PORT);
 });
-app.get("/", function (req, res, next) {
-    res.send("Loliscord API is working");
+
+app.get("/", (req, res, next) => {
+  res.send("Loliscord API is working");
 });
-var channels = {};
-var sockets = {};
+const channels: object = {};
+const sockets: object = {};
+
 /**
  * Users will connect to the signaling server, after which they'll issue a "join"
  * to join a particular channel. The signaling server keeps track of all sockets
@@ -27,63 +32,79 @@ var sockets = {};
  * information. After all of that happens, they'll finally be able to complete
  * the peer connection and will be streaming audio/video between eachother.
  */
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', (socket) => {
     socket.channels = {};
     sockets[socket.id] = socket;
-    console.log("[" + socket.id + "] connection accepted");
+    
+    console.log("["+ socket.id + "] connection accepted");
     socket.on('disconnect', function () {
         for (var channel in socket.channels) {
             part(channel);
         }
-        console.log("[" + socket.id + "] disconnected");
+        console.log("["+ socket.id + "] disconnected");
         delete sockets[socket.id];
     });
+
+
     socket.on('join', function (config) {
-        console.log("[" + socket.id + "] join ", config);
+        console.log("["+ socket.id + "] join ", config);
+
         var channel = config.channel;
         var userData = config.userData;
+
         if (channel in socket.channels) {
-            console.log("[" + socket.id + "] ERROR: already joined ", channel);
+            console.log("["+ socket.id + "] ERROR: already joined ", channel);
             return;
         }
+
         if (!(channel in channels)) {
             channels[channel] = {};
         }
-        for (var id in channels[channel]) {
-            channels[channel][id].emit('addPeer', { 'peer_id': socket.id, 'userData': userData, 'should_create_offer': false });
-            socket.emit('addPeer', { 'peer_id': id, 'userData': userData, 'should_create_offer': true });
+
+        for (let id in channels[channel]) {
+            channels[channel][id].emit('addPeer', {'peer_id': socket.id, 'userData': userData,'should_create_offer': false});
+            socket.emit('addPeer', {'peer_id': id, 'userData': userData, 'should_create_offer': true});
         }
+
         channels[channel][socket.id] = socket;
         socket.channels[channel] = channel;
     });
+
     function part(channel) {
-        console.log("[" + socket.id + "] part ");
+        console.log("["+ socket.id + "] part ");
+
         if (!(channel in socket.channels)) {
-            console.log("[" + socket.id + "] ERROR: not in ", channel);
+            console.log("["+ socket.id + "] ERROR: not in ", channel);
             return;
         }
+
         delete socket.channels[channel];
         delete channels[channel][socket.id];
-        for (var id in channels[channel]) {
-            channels[channel][id].emit('removePeer', { 'peer_id': socket.id });
-            socket.emit('removePeer', { 'peer_id': id });
+
+        for (let id in channels[channel]) {
+            channels[channel][id].emit('removePeer', {'peer_id': socket.id});
+            socket.emit('removePeer', {'peer_id': id});
         }
     }
     socket.on('part', part);
-    socket.on('relayICECandidate', function (config) {
+
+    socket.on('relayICECandidate', function(config) {
         var peer_id = config.peer_id;
         var ice_candidate = config.ice_candidate;
-        console.log("[" + socket.id + "] relaying ICE candidate to [" + peer_id + "]");
+        console.log("["+ socket.id + "] relaying ICE candidate to [" + peer_id + "]");
+
         if (peer_id in sockets) {
-            sockets[peer_id].emit('iceCandidate', { 'peer_id': socket.id, 'ice_candidate': ice_candidate });
+            sockets[peer_id].emit('iceCandidate', {'peer_id': socket.id, 'ice_candidate': ice_candidate});
         }
     });
-    socket.on('relaySessionDescription', function (config) {
+
+    socket.on('relaySessionDescription', function(config) {
         var peer_id = config.peer_id;
         var session_description = config.session_description;
-        console.log("[" + socket.id + "] relaying session description to [" + peer_id + "] ");
+        console.log("["+ socket.id + "] relaying session description to [" + peer_id + "] ");
+
         if (peer_id in sockets) {
-            sockets[peer_id].emit('sessionDescription', { 'peer_id': socket.id, 'session_description': session_description });
+            sockets[peer_id].emit('sessionDescription', {'peer_id': socket.id, 'session_description': session_description});
         }
     });
 });
