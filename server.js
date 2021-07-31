890f877d98c6c7d41a7d32355b24687025d709b8
Maple <phong.tran.maple@hcmut.edu.vn>
2021-07-31 21:17:40 +0700

    version 2

--- server.js
+++ server.js
@@ -5,18 +5,22 @@ var express = require("express");
 var app = express();
 var http = require("http");
 var server = http.createServer(app);
-var cors = require("cors");
-app.use(cors());
-var io = require('socket.io').listen(server);
-io.origins('*:*');
+
+var io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    },
+    transports: ['websocket'],
+    allowUpgrades: false
+});
+
 server.listen(PORT, null, function () {
     console.log("Listening on port " + PORT);
 });
 app.get("/", function (req, res, next) {
     res.send("Loliscord API is working");
 });
-var channels = {};
-var sockets = {};
+
 /**
  * Users will connect to the signaling server, after which they'll issue a "join"
  * to join a particular channel. The signaling server keeps track of all sockets
@@ -27,63 +31,33 @@ var sockets = {};
  * information. After all of that happens, they'll finally be able to complete
  * the peer connection and will be streaming audio/video between eachother.
  */
-io.sockets.on('connection', function (socket) {
-    socket.channels = {};
-    sockets[socket.id] = socket;
+io.on('connection', (socket) => {
     console.log("[" + socket.id + "] connection accepted");
-    socket.on('disconnect', function () {
-        for (var channel in socket.channels) {
-            part(channel);
-        }
+    socket.on('disconnect', () => {
         console.log("[" + socket.id + "] disconnected");
-        delete sockets[socket.id];
+        io.emit('removePeer', socket.id);
     });
-    socket.on('join', function (config) {
-        console.log("[" + socket.id + "] join ", config);
-        var channel = config.channel;
-        var userData = config.userData;
-        if (channel in socket.channels) {
-            console.log("[" + socket.id + "] ERROR: already joined ", channel);
-            return;
-        }
-        if (!(channel in channels)) {
-            channels[channel] = {};
-        }
-        for (var id in channels[channel]) {
-            channels[channel][id].emit('addPeer', { 'peer_id': socket.id, 'userData': userData, 'should_create_offer': false });
-            socket.emit('addPeer', { 'peer_id': id, 'userData': userData, 'should_create_offer': true });
-        }
-        channels[channel][socket.id] = socket;
-        socket.channels[channel] = channel;
+    //Get all socketIDclea
+    const peers = [];
+    io.sockets.adapter.rooms.forEach( (v, k) => {
+        peers.push(k);
     });
-    function part(channel) {
-        console.log("[" + socket.id + "] part ");
-        if (!(channel in socket.channels)) {
-            console.log("[" + socket.id + "] ERROR: not in ", channel);
-            return;
-        }
-        delete socket.channels[channel];
-        delete channels[channel][socket.id];
-        for (var id in channels[channel]) {
-            channels[channel][id].emit('removePeer', { 'peer_id': socket.id });
-            socket.emit('removePeer', { 'peer_id': id });
-        }
+    
+    if(peers.length > 1) {
+        //new comer is impolite
+        io.to(socket.id).emit('newPeer', peers, false);
+        socket.broadcast.emit('newPeer', peers, true);
     }
-    socket.on('part', part);
-    socket.on('relayICECandidate', function (config) {
-        var peer_id = config.peer_id;
-        var ice_candidate = config.ice_candidate;
-        console.log("[" + socket.id + "] relaying ICE candidate to [" + peer_id + "]");
-        if (peer_id in sockets) {
-            sockets[peer_id].emit('iceCandidate', { 'peer_id': socket.id, 'ice_candidate': ice_candidate });
-        }
+
+    socket.on('relaySDP', (sender, receiver, desc) => {
+        console.log(`[${sender}] relaying SDP to [${receiver}]`);
+        io.to(receiver).emit('SDP', sender, desc);
     });
-    socket.on('relaySessionDescription', function (config) {
-        var peer_id = config.peer_id;
-        var session_description = config.session_description;
-        console.log("[" + socket.id + "] relaying session description to [" + peer_id + "] ");
-        if (peer_id in sockets) {
-            sockets[peer_id].emit('sessionDescription', { 'peer_id': socket.id, 'session_description': session_description });
-        }
+
+    socket.on('relayICE', (sender, receiver, candidate) => {
+        console.log(`[${sender}] relaying ICE description to [${receiver}]`); 
+        io.to(receiver).emit('ICE', sender, candidate);
     });
+    
 });
+
